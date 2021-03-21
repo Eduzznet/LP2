391756563032cd8f714d04d8b69a54d142029964
Eduzznet <65985515+Eduzznet@users.noreply.github.com>
2021-03-21 04:59:42 -0300

    Update Rect.java

--- Packages/Package App/src/figures/Rect.java
+++ Packages/Package App/src/figures/Rect.java
@@ -5,12 +5,20 @@ import java.awt.*;
 public class Rect {
     int x, y;
     int w, h;
+    int r, g, b;
+    int rf, gf, bf;
 
-    public Rect (int x, int y, int w, int h) {
+    public Rect (int x, int y, int w, int h, int r, int g, int b, int rf, int gf, int bf) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.rf = rf;
+        this.gf = gf;
+        this.bf = bf;
     }
 
     public void print () {
@@ -20,6 +28,9 @@ public class Rect {
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(new Color(this.r, this.g, this.b));
         g2d.drawRect(this.x,this.y, this.w,this.h);
+        g2d.setColor(new Color(this.rf, this.gf, this.bf));
+        g2d.fillRect(this.x+1, this.y+1, this.w-1, this.h-1);
     }
 }
