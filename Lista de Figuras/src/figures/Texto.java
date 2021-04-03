5bf3a5d567ea2803908fe0f4fad857c4b02727d1
Eduzznet <65985515+Eduzznet@users.noreply.github.com>
2021-04-03 02:24:33 -0300

    Add files via upload

---
+++ Lista de Figuras/src/figures/Texto.java
@@ -0,0 +1,34 @@
+package figures;
+
+import java.awt.*;
+
+public class Texto extends Figure {
+    String texto;
+    String fonte;
+    int style;
+    int t;
+
+    public Texto (String texto, String fonte, int style, int x, int y, int t, int r, int g, int b) {
+        this.x = x;
+        this.y = y;
+        this.t = t;
+        this.texto = texto;
+        this.fonte = fonte;
+        this.style = style;
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    public void print () {
+        System.out.format("Texto de tamanho (%d) na posicao (%d,%d).\n",
+            this.t, this.x, this.y);
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setFont(new Font(fonte, style, t));
+        g2d.setPaint(new Color(this.r, this.g, this.b));
+        g2d.drawString(this.texto, this.x, this.y);
+    }
+}
